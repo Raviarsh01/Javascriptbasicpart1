952b9222cba100b4c5c3ec249f7a3a62ba7886f3
raviarsh01 <raviarsh786@gmail.com>
2022-10-02 18:34:47 +0530

    final commit

---
+++ js1.js
@@ -0,0 +1,5 @@
+let basicsal=parseInt(prompt("Enter Salary"));
+let dallow=(40/100)*basicsal;
+let hrallow=(20/100)*basicsal;
+let gsalary=basicsal+dallow+hrallow;
+console.log("The total salary is ",gsalary)
